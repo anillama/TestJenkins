2a8d0effa3263951621f1c210bd1f34da016e528
anillama <33677877+anillama@users.noreply.github.com>
2021-07-11 22:35:11 -0600

    added files

---
+++ src/com/nepal/test/Nepal.java
@@ -0,0 +1,16 @@
+package com.nepal.test;
+
+public class Nepal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Nepal");
+		System.out.println("This is a test for Jenkins");
+		
+		System.out.println("\nNew Line from Class Test Result \n");
+		TestResult ojbTest = new TestResult();
+		System.out.println(ojbTest.Test() + " From Class");
+
+	}
+
+}
---
+++ src/com/nepal/test/TestResult.java
@@ -0,0 +1,10 @@
+package com.nepal.test;
+
+public class TestResult {
+	
+	public String Test() {
+		String Name = "ANil Lama";
+		return Name;
+	}
+
+}
